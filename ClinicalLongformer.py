5c532dfc0811d7046d6564c404cb22a91931e98f
ipcoutinho <76217814+ipcoutinho@users.noreply.github.com>
2022-05-24 02:00:35 +0100

    Add files via upload

--- ClinicalLongformer.py
+++ ClinicalLongformer.py
@@ -21,28 +21,29 @@ from transformers import (
     DataCollatorWithPadding,
     EarlyStoppingCallback
 )
+# from losses import AsymmetricLossOptimized
 
 #%%
 
 print('Loading the dataset...')
 
-train_dataset = [ line.rstrip('\n') for line in codecs.open('media/ipcstorage/inputs/train.txt', encoding="utf-8") ]
-# train_dataset = [ line.rstrip('\n') for line in codecs.open('media/ipcstorage/inputs50/train50.txt', encoding="utf-8") ]
+# train_dataset = [ line.rstrip('\n') for line in codecs.open('media/ipcstorage/inputs/train.txt', encoding="utf-8") ]
+train_dataset = [ line.rstrip('\n') for line in codecs.open('media/ipcstorage/inputs50/train50.txt', encoding="utf-8") ]
 
-val_dataset = [ line.rstrip('\n') for line in codecs.open('media/ipcstorage/inputs/dev.txt', encoding="utf-8") ]
-# val_dataset = [ line.rstrip('\n') for line in codecs.open('media/ipcstorage/inputs50/dev50.txt', encoding="utf-8") ]
+# val_dataset = [ line.rstrip('\n') for line in codecs.open('media/ipcstorage/inputs/dev.txt', encoding="utf-8") ]
+val_dataset = [ line.rstrip('\n') for line in codecs.open('media/ipcstorage/inputs50/dev50.txt', encoding="utf-8") ]
 
 #%%
 
 print('Processing the labels...')
 
-num_labels = 8921 #50
+num_labels = 50 #8921
 
-train_1hot = np.load('media/ipcstorage/inputs/train_1hot.npz')['arr_0']
-# train_1hot = np.load('media/ipcstorage/inputs50/train50_1hot.npz')['arr_0']
+# train_1hot = np.load('media/ipcstorage/inputs/train_1hot.npz')['arr_0']
+train_1hot = np.load('media/ipcstorage/inputs50/train50_1hot.npz')['arr_0']
 
-val_1hot = np.load('media/ipcstorage/inputs/val_1hot.npz')['arr_0']
-# val_1hot = np.load('media/ipcstorage/inputs50/val50_1hot.npz')['arr_0']
+# val_1hot = np.load('media/ipcstorage/inputs/val_1hot.npz')['arr_0']
+val_1hot = np.load('media/ipcstorage/inputs50/val50_1hot.npz')['arr_0']
 
 #%%
 
@@ -88,7 +89,7 @@ class MyDataset(torch.utils.data.Dataset):
         max_length = compute_max_length(aux)
         encodings = tokenizer(self.texts[idx], truncation=True, padding='max_length', max_length=max_length)
         item = {k: torch.tensor(v) for k, v in encodings.items()}
-        item["labels"] = torch.tensor(self.labels[idx])
+        item['labels'] = torch.tensor(self.labels[idx])
         return item
 
     def __len__(self):
@@ -107,7 +108,7 @@ model = LongformerForSequenceClassification.from_pretrained(model_name, num_labe
 #%%
 
 training_args = TrainingArguments(
-    output_dir='media/ipcstorage/results_ClinicalLongformer', 
+    output_dir='media/ipcstorage/results_ClinicalLongformer50_sparsemax', 
     group_by_length=True,
     learning_rate=2e-5,
     lr_scheduler_type='constant',
@@ -124,38 +125,109 @@ training_args = TrainingArguments(
 
 #%%
 
-def sigmoid(x):
-  return 1 / (1 + np.exp(-x))
+# def sigmoid(x):
+#   return 1 / (1 + np.exp(-x))
 
-file = open('media/ipcstorage/metrics_ClinicalLongformer.txt','w')
+# file = open('media/ipcstorage/metrics_ClinicalLongformer_ASL.txt','w')
+
+# def compute_metrics(pred):
+#     labels = pred.label_ids
+#     logits = pred.predictions
+#     preds = np.round(sigmoid(logits))
+
+#     # Loss
+#     # loss = torch.nn.BCEWithLogitsLoss(reduction='sum')(torch.Tensor(logits),torch.Tensor(labels))
+#     # loss = AsymmetricLossOptimized()(torch.tensor(logits), torch.tensor(labels))
+
+#     # Metrics
+#     f1_macro = f1_score(labels, preds, average='macro', zero_division=0)
+#     f1_micro = f1_score(labels, preds, average='micro', zero_division=0)
+#     metrics = 'loss: ' + str(loss.item()) + ', f1_macro: ' + str(f1_macro) +  ', f1_micro: ' + str(f1_micro) + '\n'
+#     file.write(metrics)
+#     return {
+#         'f1_macro': f1_macro, 'f1_micro': f1_micro
+#     }
+
+#%%
+
+file = open('media/ipcstorage/metrics_ClinicalLongformer50_sparsemax.txt','w')
 
 def compute_metrics(pred):
-    labels = pred.label_ids
-    logits = pred.predictions
-    preds = np.round(sigmoid(logits))
-
-    # Loss
-    loss = torch.nn.BCEWithLogitsLoss(reduction='sum')(torch.Tensor(logits),torch.Tensor(labels))
-
+    labels = torch.Tensor(pred.label_ids).to('cuda')
+    logits = torch.Tensor(pred.predictions).to('cuda')
+    probs, tau, _ =  project_onto_simplex(logits)
+    preds = compute_support(probs)
+    
     # Metrics
-    f1_macro = f1_score(labels, preds, average='macro', zero_division=0)
-    f1_micro = f1_score(labels, preds, average='micro', zero_division=0)
-    metrics = 'loss: ' + str(loss.item()) + ', f1_macro: ' + str(f1_macro) +  ', f1_micro: ' + str(f1_micro) + '\n'
+    f1_micro = f1_score(labels.cpu().detach().numpy(), preds.cpu().detach().numpy(), average='micro', zero_division=0)
+    metrics = 'f1_micro: ' + str(f1_micro) + '\n'
     file.write(metrics)
     return {
-        'f1_macro': f1_macro, 'f1_micro': f1_micro
+        'f1_micro': f1_micro
     }
 
 #%%
 
+# class MultilabelTrainer(Trainer):
+#     def compute_loss(self, model, inputs, return_outputs = False):
+#         labels = inputs.pop('labels')
+#         outputs = model(**inputs)
+#         logits = outputs.logits
+#         # loss = torch.nn.BCEWithLogitsLoss(reduction='sum')(logits,labels)
+#         loss = AsymmetricLossOptimized()(logits, labels)
+#         return (loss, outputs) if return_outputs else loss
+
+#%%
+
+def compute_support(probs):
+    supp = torch.zeros(probs.shape)
+    supp[probs.nonzero(as_tuple=True)] = 1.
+    return supp
+
+def project_onto_simplex(a, radius=1.0):
+    '''Project point a to the probability simplex.
+    Returns the projected point x and the residual value.'''
+    x0 = a.clone().detach()
+    batch_size, d = x0.shape
+    y0, ind_sort = torch.sort(-x0)
+    ycum = torch.cumsum(y0,dim=-1)
+    val = 1.0/torch.arange(1,d+1).to('cuda') * (ycum - radius)
+
+    rho = torch.zeros((batch_size,1),dtype=torch.long).to('cuda')
+    tau = torch.zeros((batch_size,1)).to('cuda')
+    for batch in range(batch_size):
+        ind = torch.nonzero(y0[batch] > val[batch])
+        rho[batch] = ind[-1]
+        tau[batch] = val[batch,rho[batch]]
+        
+    y = y0 - tau
+    x = x0.clone().detach()
+    for batch in range(batch_size):
+        ind = torch.nonzero(y[batch] < 0)
+        y[batch,ind] = 0
+        x[batch,ind_sort[batch]] = y[batch]
+
+    return x, tau, (.5*torch.bmm((x-a).view(batch_size,1,d), (x-a).view(batch_size,d,1))).squeeze(1)
+
 class MultilabelTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs = False):
         labels = inputs.pop('labels')
+        gold_labels = compute_support(labels).to('cuda')
+
         outputs = model(**inputs)
         logits = outputs.logits
-        loss = torch.nn.BCEWithLogitsLoss(reduction='sum')(logits,labels)
-        return (loss, outputs) if return_outputs else loss
 
+        batch_size,len = logits.shape
+
+        probs, tau, _ =  project_onto_simplex(logits)
+        predicted_labels = compute_support(probs).to('cuda')
+        
+        loss_t = \
+                (-torch.bmm(logits.view(batch_size,1,len),labels.view(batch_size,len,1)) + .5*torch.bmm((logits**2 - tau**2).view(batch_size,1,len),predicted_labels.view(batch_size,len,1))).squeeze(1) + .5/torch.sum(gold_labels,dim=-1).unsqueeze(1)
+
+        loss = sum(loss_t)[0]
+        return (loss, outputs) if return_outputs else loss
+    
 #%%
 
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer, padding='longest')
